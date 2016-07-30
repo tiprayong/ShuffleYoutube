14ba9735160952daf5f0ec7987a27b85b877cff9
tiprayong <tippsvita@gmail.com>
2016-07-30 19:31:45 +0700

    Update PlayYoutube.java

--- app/src/main/java/com/comtip/shuffleyoutube/PlayYoutube.java
+++ app/src/main/java/com/comtip/shuffleyoutube/PlayYoutube.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
  * Created by TipRayong on 20/7/2559.
  */
 public class PlayYoutube extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
-    final String YOUTUBE_API_KEY = "AIzaSyB-iYo9CZ0yb13a4esvDeVOZG2zdTqFf0I";
+    final String YOUTUBE_API_KEY = "Youtube API Key";
     YouTubePlayerView youtubeView;
     ArrayList<String> shuffle = new ArrayList<>();
     TextView headerVideo;
